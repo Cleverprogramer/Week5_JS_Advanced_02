03ec1ccec00c379142efb08db7b421af3d25a935
cleverprogrammer <cleverprogrammer.com@gmail.com>
2023-03-09 10:55:07 +0000

    i done the whole assignment

--- classes.js
+++ classes.js
@@ -1,8 +1,6 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several classes with their correct inheritance hierarchy.
-
   In this file you will be creating three classes: GameObject, CharacterStats, Humanoid.  
-
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your classes.
   
   Each class has unique properties and methods that are defined in their block comments below:
@@ -37,12 +35,41 @@ console.log("This is example result: ", fred.task());
   * destroy() // A method that returns: `${this.name} was removed from the game.`
 */
 
+class GameObject {
+  constructor(attr){
+    this.createdAt = attr.createdAt
+    this.name = attr.name
+    this.dimensions = attr.dimensions
+  }
+
+destroy(){
+  return `${this.name} was removed from the game.`
+}
+
+}
+
+
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // A method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's method
 */
+
+class CharacterStats extends GameObject{
+  constructor(attr){
+    super(attr)
+    this.healthPoints = attr.healthPoints
+  }
+
+  takeDamage(){
+    return `${this.name} took damage`
+  }
+
+}
+
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -54,6 +81,18 @@ console.log("This is example result: ", fred.task());
   * should inherit takeDamage() from CharacterStats
 */
  
+class Humanoid extends CharacterStats{
+  constructor(attr){
+    super(attr)
+    this.team = attr.team
+    this.weapons = attr.weapons
+    this.language = attr.language
+  }
+  greet(){
+    return `${this.name} offers a greeting in ${this.language}`
+  }
+ }
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -62,7 +101,7 @@ console.log("This is example result: ", fred.task());
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -123,7 +162,7 @@ console.log("This is example result: ", fred.task());
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+  
 
   // Stretch task: 
   // * Create Villain and Hero class that inherit from the Humanoid class.  
